0b7721027bce8cdb20eb7d3e48da77fee96751eb
Manuel Meurer <manuel@meurer.io>
2022-03-11 13:14:49 +0100

    hack hack hack

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,10 @@
 import "index.css"
 
-// Import all JavaScript & CSS files from src/_components
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
 
-console.info("Bridgetown is loaded!")
+// Redirect to canonical URL
+// var origin = window.location.protocol + '//' + window.location.host;
+// if (origin != "manuelmeurer.com") {
+//   var correctUrl = window.location.href.replace(origin, "manuelmeurer.com");
+//   window.location.replace(correctUrl);
+// }
