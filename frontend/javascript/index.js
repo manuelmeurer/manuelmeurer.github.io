5d01b20ece6b6612a138911ca6cd583febf80523
Manuel Meurer <manuel@meurer.io>
2022-03-17 17:40:34 +0100

    no need to redirect

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,10 +1,3 @@
 import "index.css"
 
 import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
-
-// Redirect to canonical URL
-// var origin = window.location.protocol + '//' + window.location.host;
-// if (origin != "manuelmeurer.com") {
-//   var correctUrl = window.location.href.replace(origin, "manuelmeurer.com");
-//   window.location.replace(correctUrl);
-// }
